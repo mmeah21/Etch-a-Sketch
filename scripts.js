4898fc609abc818fb58486ba6ce73a34d4dd2f5f
mmeah21 <maevms@live.com>
2022-06-28 06:02:55 -0400

    Grid Created adjustable grid created using javascript only

--- scripts.js
+++ scripts.js
@@ -4,8 +4,10 @@
 
 //pad.textContent = 'test';
 //testing pad content
- createSquares(pad,16);
+const numberOfSquares = 30;
+ createSquares(pad,numberOfSquares);
  const squares = document.querySelectorAll('div.square');
+ 
 
 
 
@@ -13,10 +15,10 @@
 
 //container.innerHTML +='<div class="square"></div>';
  //creates squares for a container
- function createSquares (container,totalSquares){
+ function createSquares (container,oneLine){
+    var area = oneLine*oneLine
 
-
-    for (var i = 0;i < totalSquares; i++) {
+    for (var i = 0;i < area; i++) {
         container.innerHTML +='<div class="square"></div>';
     }
  }
@@ -26,11 +28,35 @@ let squareList = Array.from(squares);
 setIdSquares(squareList);
 
 
-
 function setIdSquares(arr){
     for(var i = 0; i < arr.length; i++){
-        arr[i].id=i+1;
+        arr[i].id = i+1;
     }
 }
 
+function toPercentDimensions(num1){
+    let ans=((.8/num1)*100);
+    return ans+'%';
+}
+
+function toPercentBorder(num1){
+    let ans=((0.2/num1)*100);
+    return ans+'%';
+}
+
+
+
+//adjust width and height of squares to total
+
+function adjusted (){
+    document.querySelectorAll('.square').forEach(div => {
+    div.style.height = toPercentDimensions(numberOfSquares);
+    div.style.width = toPercentDimensions(numberOfSquares);
+    div.style.borderWidth == toPercentBorder(numberOfSquares);
+    })
+};
+console.log(toPercentDimensions(numberOfSquares));
+console.log(toPercentBorder(numberOfSquares));
+console.log(numberOfSquares);
+adjusted();
 
